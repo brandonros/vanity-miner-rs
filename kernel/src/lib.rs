d25bd98fd2d145d3fc28f37a5b0131abbb7f660b
Brandon Ros <brandonros1@gmail.com>
2025-04-20 02:06:16 +0000

    random seed problem

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,9 +1,12 @@
 extern crate alloc;
 
+mod mock_rng;
+
 use cuda_std::prelude::*;
 use ed25519_compact::{PublicKey, SecretKey, ge_scalarmult_base};
 use hmac_sha512::Hash;
-use gpu_rand::xoroshiro::Xoroshiro128StarStar;
+//use gpu_rand::xoroshiro::Xoroshiro128StarStar;
+use crate::mock_rng::MockXoroshiro128StarStar; 
 use gpu_rand::xoroshiro::rand_core::SeedableRng;
 use gpu_rand::xoroshiro::rand_core::RngCore;
 use bs58;
@@ -11,48 +14,37 @@ use bs58;
 #[kernel]
 #[allow(improper_ctypes_definitions, clippy::missing_safety_doc)]
 pub unsafe fn vecadd(a: &[f32], b: &[f32], c: *mut f32) {
-    // generate random input
-    let mut rng_seed = 42;
-    let num_iterations = 1;
+    let mut rng_seed: u64 = 42; // TODO: make this unique for each thread?
+    let mut num_iterations: u64 = 0;
 
-    for _ in 0..num_iterations {
+    loop {
         // generate random input for seed
-        println!("generating random input");
-        let mut rng = Xoroshiro128StarStar::seed_from_u64(rng_seed);
+        let mut rng = MockXoroshiro128StarStar::seed_from_u64(rng_seed);
         let mut input = [0u8; 32];
         rng.fill_bytes(&mut input);
-        println!("generated random input");
 
         // sha512 hash input
-        println!("hashing input");
         let mut hash = Hash::new();
         hash.update(&input[0..32]);
         let mut hashed_input = hash.finalize();
-        println!("hashed input");
 
         // Apply Ed25519 clamping
-        println!("clamping input");
         hashed_input[0] &= 248;
         hashed_input[31] = (hashed_input[31] & 127) | 64;
-        println!("clamped input");
 
         // Now use the library's ge_scalarmult_base to compute the public key
         // from the first 32 bytes of your hashed and clamped input
         // Use the library's function to compute public key from scalar
-        println!("computing public key");
         let mut scalar = [0u8; 32];
         scalar.copy_from_slice(&hashed_input[0..32]);
         let public_key_bytes = ge_scalarmult_base(&scalar).to_bytes();
-        println!("computed public key");
 
         // bs58 encode public key
-        println!("encoding public key");
         let mut bs58_encoded_public_key = [0; 64];
         bs58::encode(&public_key_bytes[..32]).onto(&mut bs58_encoded_public_key[..]).unwrap();
-        println!("encoded public key");
 
-        // check if public key starts with 31313131
-        if bs58_encoded_public_key[0] == 0x31 && bs58_encoded_public_key[1] == 0x31 {
+        // check if public key starts with a
+        if bs58_encoded_public_key[0] == 0x61 {
             println!("found something");
 
             // build a secret key
@@ -62,12 +54,16 @@ pub unsafe fn vecadd(a: &[f32], b: &[f32], c: *mut f32) {
             combined_key[32..64].copy_from_slice(&public_key_bytes);
             let secret_key = SecretKey::new(combined_key);*/
 
-            println!("input: {:02x?}", input);
+            /*println!("input: {:02x?}", input);
             println!("Public key: {:02x?}", public_key_bytes);
-            println!("Base58 encoded public key: {:02x?}", bs58_encoded_public_key);
+            println!("Base58 encoded public key: {:02x?}", bs58_encoded_public_key);*/
             break;
-        } else {
-            println!("no match");
+        }
+
+        num_iterations += 1;
+
+        if num_iterations % 1000 == 0 {
+            println!("num_iterations: {}", num_iterations);
         }
 
         rng_seed += 1;
