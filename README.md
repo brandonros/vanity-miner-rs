77915baf93b93ff60f6babd2ea60fb966a236b85
Brandon Ros <brandonros1@gmail.com>
2025-04-20 17:46:16 +0000

    no submodule

--- README.md
+++ README.md
@@ -4,6 +4,15 @@ Ed25519 vanity generator with CUDA in Rust
 ## How to use
 
 ```shell
+# from vastai/base-image:cuda-12.8.1-cudnn-devel-ubuntu24.04
+
+# configure ssh key
+ssh-keygen -t rsa -b 3072 -f ~/.ssh/id_rsa -N ""
+
+# configure git
+git config --global user.name "Brandon Ros"
+git config --global user.email $EMAIL
+
 # install system dependencies
 apt update
 apt-get remove --purge 'cuda-12-0*' 'cuda-*12-0*' 'libcublas-12-0' 'libcublas-dev-12-0' cuda-12.0
@@ -16,6 +25,8 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # build
 export LLVM_CONFIG="llvm-config-7"
+export LLVM_LINK_STATIC=1
+export RUST_LOG=info
 export LD_LIBRARY_PATH="/usr/local/cuda/nvvm/lib64/"
 cargo build
 ```
--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,68 +1,49 @@
-extern crate alloc;
-
-mod mock_rng;
-
-use cuda_std::prelude::*;
-use ed25519_compact::{PublicKey, SecretKey, ge_scalarmult_base};
-use hmac_sha512::Hash;
-use gpu_rand::xoroshiro::Xoroshiro128StarStar;
-use crate::mock_rng::MockXoroshiro128StarStar; 
-use gpu_rand::xoroshiro::rand_core::SeedableRng;
-use gpu_rand::xoroshiro::rand_core::RngCore;
+use ed25519_compact::ge_scalarmult_base;
+use ed25519_compact::sha512::Hash;
+use rand_xoshiro::rand_core::{RngCore as _, SeedableRng};
+use rand_xoshiro::Xoshiro256StarStar;
 use bs58;
 
 #[kernel]
 #[allow(improper_ctypes_definitions, clippy::missing_safety_doc)]
-pub unsafe fn find_private_key(a: &[u8], out_ptr: *mut u8) {
+pub fn find_vanity_private_key(vanity_prefix: &[u8], rng_seed: u64) {
+    let vanity_prefix_len = vanity_prefix.len();
+
     let idx = thread::index_1d() as usize;
+    let mut rng = Xoshiro256StarStar::seed_from_u64(rng_seed + idx as u64);
+    let mut hasher = Hash::new();
+    let mut private_key = [0u8; 32];
+    let mut bs58_encoded_public_key = [0u8; 44];
+    let mut num_iterations = 0;
 
-    let mut rng_seed: u64 = 42; // TODO: make this unique for each thread?
-    let mut num_iterations: u64 = 0;
-
-    for _ in 0..10000 {
-        // generate random input for seed
-        let mut rng = MockXoroshiro128StarStar::seed_from_u64(rng_seed);
-        let mut input = [0u8; 32];
-        rng.fill_bytes(&mut input);
+    loop {
+        // generate random input
+        rng.fill_bytes(&mut private_key[0..32]);
 
         // sha512 hash input
-        let mut hash = Hash::new();
-        hash.update(&input[0..32]);
-        let mut hashed_input = hash.finalize();
+        hasher.reset();
+        hasher.update(&private_key[0..32]);
+        let mut hashed_private_key = hasher.finalize();
 
-        // Apply Ed25519 clamping
-        hashed_input[0] &= 248;
-        hashed_input[31] = (hashed_input[31] & 127) | 64;
+        // apply ed25519 clamping
+        hashed_private_key[0] &= 248;
+        hashed_private_key[31] = (hashed_private_key[31] & 127) | 64;
 
-        // Now use the library's ge_scalarmult_base to compute the public key
-        // from the first 32 bytes of your hashed and clamped input
-        // Use the library's function to compute public key from scalar
-        let mut scalar = [0u8; 32];
-        scalar.copy_from_slice(&hashed_input[0..32]);
-        let public_key_bytes = ge_scalarmult_base(&scalar).to_bytes();
+        // ed25519 private key -> public key (first 32 bytes only)
+        let public_key_bytes = ge_scalarmult_base(&hashed_private_key[0..32]).to_bytes();
 
         // bs58 encode public key
-        let mut bs58_encoded_public_key = [0; 64];
-        bs58::encode(&public_key_bytes[..32]).onto(&mut bs58_encoded_public_key[..]).unwrap();
+        bs58::encode(&public_key_bytes[0..32]).onto(&mut bs58_encoded_public_key[0..]).unwrap();
 
-        // check if public key starts with a
-        if bs58_encoded_public_key[0] == 0x61 {
-            println!("found something");
-
-            // build a secret key
-            /*let public_key = PublicKey::new(public_key_bytes);
-            let mut combined_key = [0u8; 64];
-            combined_key[0..32].copy_from_slice(&input); // Original input as seed
-            combined_key[32..64].copy_from_slice(&public_key_bytes);
-            let secret_key = SecretKey::new(combined_key);*/
-
-            println!("input: {:02x?}", input);
+        // check if public key starts with vanity prefix
+        if bs58_encoded_public_key[0..vanity_prefix_len] == *vanity_prefix {
+            println!("found match");
+            println!("Private key: {:02x?}", private_key);
             println!("Public key: {:02x?}", public_key_bytes);
             println!("Base58 encoded public key: {:02x?}", bs58_encoded_public_key);
             break;
         }
-    }
 
-    let out = unsafe { core::ptr::slice_from_raw_parts_mut(out_ptr, 32) };
-    (*out)[0] = 123;
+        num_iterations += 1;
+    }
 }
