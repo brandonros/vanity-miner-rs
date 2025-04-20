f0a05e8f0b0f8e0f65d4d9f858aadd0ee4271fd8
Brandon Ros <brandonros1@gmail.com>
2025-04-20 18:42:17 +0000

    almost works

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -4,23 +4,47 @@ extern crate alloc;
 
 use ed25519_compact::ge_scalarmult_base;
 use ed25519_compact::sha512::Hash;
-use rand_xoshiro::rand_core::{RngCore as _, SeedableRng};
-use rand_xoshiro::Xoshiro256StarStar;
+use rand_core::{SeedableRng, RngCore};
+use rand_xorshift::XorShiftRng;
 use bs58;
 
 #[cuda_std::kernel]
 #[allow(improper_ctypes_definitions, clippy::missing_safety_doc)]
-pub unsafe fn find_vanity_private_key(vanity_prefix_ptr: *const u8, vanity_prefix_len: usize, rng_seed: u64) {
+pub unsafe fn find_vanity_private_key(
+    vanity_prefix_ptr: *const u8, 
+    vanity_prefix_len: usize, 
+    rng_seed: u64,
+    found_flag: *mut u8
+) {
+    // read vanity prefix from host
     let vanity_prefix = core::slice::from_raw_parts(vanity_prefix_ptr, vanity_prefix_len as usize);
 
+    // get thread index
     let idx = cuda_std::thread::index_1d() as usize;
-    let mut rng = Xoshiro256StarStar::seed_from_u64(rng_seed + idx as u64);
-    let mut hasher = Hash::new();
+
+    // initialize rng
+    let mut rng = XorShiftRng::seed_from_u64(rng_seed + idx as u64);
+
+    // initialize buffers
     let mut private_key = [0u8; 32];
     let mut bs58_encoded_public_key = [0u8; 44];
     let mut num_iterations = 0;
 
+    // initialize hasher
+    let mut hasher = Hash::new();
+
+    // loop until match is found
     loop {
+        // check if match has been found in another thread
+        if num_iterations % 1000 == 0 && *found_flag != 0 {
+            break;
+        }
+
+        if num_iterations >= 100 {
+            *found_flag = 1;
+            break;
+        }
+
         // generate random input
         rng.fill_bytes(&mut private_key[0..32]);
 
@@ -41,10 +65,10 @@ pub unsafe fn find_vanity_private_key(vanity_prefix_ptr: *const u8, vanity_prefi
 
         // check if public key starts with vanity prefix
         if bs58_encoded_public_key[0..vanity_prefix_len] == *vanity_prefix {
-            /*cuda_std::println!("found match");
+            cuda_std::println!("found match");
             cuda_std::println!("Private key: {:02x?}", private_key);
             cuda_std::println!("Public key: {:02x?}", public_key_bytes);
-            cuda_std::println!("Base58 encoded public key: {:02x?}", bs58_encoded_public_key);*/
+            cuda_std::println!("Base58 encoded public key: {:02x?}", bs58_encoded_public_key);
             break;
         }
 
--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,19 @@ use cudarc::{
 
 fn main() -> Result<(), DriverError> {
     // Define the vanity prefix we're looking for
-    let vanity_prefix    = b"aaaa";
+    let vanity_prefix    = b"aa";
     let vanity_prefix_len: usize = vanity_prefix.len();
     let rng_seed: u64 = 2457272140905572020; // You might want to generate this randomly
+    let mut found_flag = [0u8; 1];
 
     // Initialize CUDA context and get default stream
     let ctx = CudaContext::new(0)?;
     let stream = ctx.default_stream();
 
     // Load the pre-compiled PTX that was generated during build
+    println!("Loading module...");
     let module = ctx.load_module(Ptx::from_src(include_str!(concat!(env!("OUT_DIR"), "/kernel.ptx"))))?;
     let f = module.load_function("find_vanity_private_key").unwrap();
-
-    println!("Launching kernel...");
 
     // Configure kernel launch parameters
     let block_size = 256; // Threads per block
@@ -27,19 +27,23 @@ fn main() -> Result<(), DriverError> {
         block_dim: (block_size, 1, 1),
         shared_mem_bytes: 0,
     };
+
+    println!("Copying to device...");
     
     // Copy vanity prefix to device
     let vanity_prefix_dev    = stream.memcpy_stod(vanity_prefix)?;
-
+    let found_flag_dev       = stream.memcpy_stod(&found_flag)?;
     // Launch the kernel
     let mut launch_args = stream.launch_builder(&f);
     launch_args.arg(&vanity_prefix_dev);
     launch_args.arg(&vanity_prefix_len);
     launch_args.arg(&rng_seed);
+    launch_args.arg(&found_flag_dev);
 
     println!("Launching kernel...");
     unsafe { launch_args.launch(cfg) }?;
 
+    println!("Synchronizing...");
     stream.synchronize()?;
 
     Ok(())
