59dda429e8ab3e79b40e2a3f5fbe704d5c9e9c3d
Brandon Ros <brandonros1@gmail.com>
2025-05-03 20:10:28 +0000

    need to remove dead code

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -3,6 +3,7 @@
 extern crate alloc;
 
 mod sha512;
+mod edwards25519;
 
 use rand_core::{SeedableRng, RngCore};
 use rand_xorshift::XorShiftRng;
@@ -19,7 +20,7 @@ fn sha512_compact(input: &[u8]) -> [u8; 64] {
 fn derrive_public_key_compact(hashed_private_key_bytes: [u8; 64]) -> [u8; 32] {
     let mut input = [0u8; 32];
     input.copy_from_slice(&hashed_private_key_bytes[0..32]);
-    let public_key_bytes = ge_scalarmult_base(&input).to_bytes();
+    let public_key_bytes = crate::edwards25519::ge_scalarmult_base(&input).to_bytes();
     public_key_bytes
 }
 
