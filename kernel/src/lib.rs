4394e979f49711cb87f367a73e1c3eddb3710167
Brandon Ros <brandonros1@gmail.com>
2025-05-24 20:36:19 -0400

    one function at  a time

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -34,8 +34,8 @@ fn derrive_public_key(hashed_private_key_bytes: [u8; 64]) -> [u8; 32] {
         let mut input = [0u8; 32];
         input.copy_from_slice(&hashed_private_key_bytes[0..32]);
         let scalar = curve25519_dalek::Scalar::from_bytes_mod_order(input);
-        /*let point = curve25519_dalek::constants::ED25519_BASEPOINT_TABLE * &scalar;
-        let compressed_point = point.compress();
+        let point = curve25519_dalek::constants::ED25519_BASEPOINT_TABLE * &scalar;
+        /*let compressed_point = point.compress();
         let public_key_bytes = compressed_point.to_bytes();
         public_key_bytes*/
         [0u8; 32]
