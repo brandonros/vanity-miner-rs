6d16d4c7b7f1a0b07a24f60ee74404cc5decf51b
Brandon Ros <brandonros1@gmail.com>
2025-05-26 15:25:29 -0400

    thread idx

--- kernel/src/xorshiro.rs
+++ kernel/src/xorshiro.rs
@@ -39,7 +39,7 @@ pub fn generate_random_private_key(thread_idx: usize, rng_seed: u64) -> [u8; 32]
     use rand_core::{SeedableRng, RngCore};
     use rand_xoshiro::Xoroshiro128StarStar;
     let mut private_key = [0u8; 32];
-    let mut rng = Xoroshiro128StarStar::seed_from_u64(rng_seed);
+    let mut rng = Xoroshiro128StarStar::seed_from_u64(rng_seed + thread_idx as u64);
     rng.fill_bytes(&mut private_key);
     private_key
 }
