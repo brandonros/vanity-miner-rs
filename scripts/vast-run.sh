5955c7c3032731dac2fe5a3203ce7b8cef85f331
Brandon Ros <brandonros1@gmail.com>
2025-06-06 02:13:20 +0000

    gpu runner

--- scripts/vast-run.sh
+++ scripts/vast-run.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-PORT=27992
+PORT=39920
 HOST=ssh4.vast.ai
 USER=root
 
 ssh -o StrictHostKeyChecking=no -p $PORT $USER@$HOST <<'EOF'
-if [[ ! -f ed25519_vanity ]]
+if [[ ! -f gpu_runner ]]
 then
-  curl -L -O https://github.com/brandonros/ed25519-vanity-rs/releases/download/1.1.0/ed25519_vanity
-  chmod +x ed25519_vanity
+  curl -L -O https://github.com/brandonros/ed25519-vanity-rs/releases/download/1.2.0/gpu_runner
+  chmod +x gpu_runner
 fi
-./ed25519_vanity Muffin ""
+./gpu_runner shallenge brandonros 00000000002a6043ad35708ef4e05c7f8d64c5a0e74d71312886fdc4b41eee0f
 EOF
