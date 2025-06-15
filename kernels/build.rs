2f52b12e8ffa4a9cf0c480303b3138e88f9c5b92
Brandon Ros <brandonros1@gmail.com>
2025-06-15 17:13:30 -0400

    logger

--- kernels/build.rs
+++ kernels/build.rs
@@ -4,27 +4,7 @@ use std::path;
 use cuda_builder::CudaBuilder;
 
 fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=../kernels");
-    println!("cargo:rerun-if-changed=../common");
-    println!("cargo:rerun-if-changed=../logic");
-
-    unsafe {
-        env::set_var("LLVM_CONFIG", "llvm-config-19");
-        env::set_var("LLVM_LINK_STATIC", "1");
-        env::set_var("RUST_LOG", "info");
-
-        let cuda_lib_path = "/usr/local/cuda/nvvm/lib64/";
-        match env::var("LD_LIBRARY_PATH") {
-            Ok(existing_path) => {
-                env::set_var("LD_LIBRARY_PATH", format!("{}:{}", existing_path, cuda_lib_path));
-            }
-            Err(_) => {
-                env::set_var("LD_LIBRARY_PATH", cuda_lib_path);
-            }
-        }
-    }
-
+    env_logger::init();
     let out_path = path::PathBuf::from(env::var("OUT_DIR").unwrap());
     CudaBuilder::new("./")
         .copy_to(out_path.join("kernels.ptx"))
