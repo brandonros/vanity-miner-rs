314b5fdd2c9c45536799de27a4d93e09d2ff893e
Brandon Ros <brandonros1@gmail.com>
2025-06-19 23:38:40 -0400

    tweak script

--- transpiler/scripts/run.sh
+++ transpiler/scripts/run.sh
@@ -2,8 +2,23 @@
 
 . ~/.bash_profile
 
+# clean
+cargo clean
+
+# build kernels
+pushd ../kernels
+cargo build --release || exit 1
+popd
+
+# find the .ll file
+LL_FILE=$(find $CARGO_TARGET_DIR/riscv64gc-unknown-none-elf/release/deps/kernels-* -type f -name "*.ll")
+if [ -z "$LL_FILE" ]; then
+    echo "No .ll file found"
+    exit 1
+fi
+
 # generate the .ll file
-cargo run || exit 1
+cargo run --release -- $LL_FILE || exit 1
 
 # convert the .ll files to .bc files
 llvm-as /tmp/output.ll -o /tmp/output.bc
@@ -14,6 +29,6 @@ scp /tmp/output.bc brandon@asusrogstrix.local:
 scp /tmp/libintrinsics.bc brandon@asusrogstrix.local:
 
 # compile the .bc files
-ssh brandon@asusrogstrix.local "cd /home/brandon/nvvm_compiler && ./compile.sh && ./nvvm_compiler output.bc libintrinsics.bc > output.ptx"
-ssh brandon@asusrogstrix.local "cd /home/brandon/nvvm_compiler && ptxas -arch=sm_120 -o output.cubin output.ptx"
-ssh brandon@asusrogstrix.local "cd /home/brandon/nvvm_compiler && nvcc -fatbin -arch=sm_120 -o output.fatbin output.ptx"
+ssh brandon@asusrogstrix.local "cd /home/brandon/nvvm_compiler && ./compile.sh && ./nvvm_compiler output.bc libintrinsics.bc > output.ptx" || exit 1
+ssh brandon@asusrogstrix.local "cd /home/brandon/nvvm_compiler && ptxas -arch=sm_120 -o output.cubin output.ptx" || exit 1
+ssh brandon@asusrogstrix.local "cd /home/brandon/nvvm_compiler && nvcc -fatbin -arch=sm_120 -o output.fatbin output.ptx" || exit 1
