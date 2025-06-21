785a7529cffa2aef0d5326f6c40466129a5a28b8
Brandon Ros <brandonros1@gmail.com>
2025-06-21 10:49:32 -0400

    transpiler script

--- nvvm_compiler/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cc main.c -I/usr/local/cuda-12.9/nvvm/include -L/usr/local/cuda-12.9/nvvm/lib64 -lnvvm -o nvvm_compiler
--- transpiler/scripts/run.sh
+++ transpiler/scripts/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-. ~/.bash_profile
-
 # clean
 cargo clean
 
@@ -17,18 +15,21 @@ if [ -z "$LL_FILE" ]; then
     exit 1
 fi
 
+# replace uwtable attributes with no_uwtable attributes due to riscv core being built with unwind?
+sed -i 's/ uwtable //g' $LL_FILE
+sed -i 's/ uwtable//g' $LL_FILE
+
 # generate the .ll file
 cargo run --release -- $LL_FILE || exit 1
 
 # convert the .ll files to .bc files
-llvm-as /tmp/output.ll -o /tmp/output.bc
-llvm-as assets/libintrinsics.ll -o /tmp/libintrinsics.bc
-
-# copy the .bc files to the nvvm_compiler directory
-scp /tmp/output.bc brandon@asusrogstrix.local:
-scp /tmp/libintrinsics.bc brandon@asusrogstrix.local:
+llvm-as-19 /tmp/output.ll -o /tmp/output.bc || exit 1
+llvm-as-19 assets/libintrinsics.ll -o /tmp/libintrinsics.bc || exit 1
 
 # compile the .bc files
-ssh brandon@asusrogstrix.local "cd /home/brandon/nvvm_compiler && ./compile.sh && ./nvvm_compiler output.bc libintrinsics.bc > output.ptx" || exit 1
-ssh brandon@asusrogstrix.local "cd /home/brandon/nvvm_compiler && ptxas -arch=sm_120 -o output.cubin output.ptx" || exit 1
-ssh brandon@asusrogstrix.local "cd /home/brandon/nvvm_compiler && nvcc -fatbin -arch=sm_120 -o output.fatbin output.ptx" || exit 1
+pushd ../nvvm_compiler
+make || exit 1
+./build/nvvm_compiler /tmp/output.bc /tmp/libintrinsics.bc > /tmp/output.ptx || exit 1
+ptxas -arch=sm_120 -o /tmp/output.cubin /tmp/output.ptx || exit 1
+nvcc -fatbin -arch=sm_120 -o /tmp/output.fatbin /tmp/output.ptx || exit 1
+popd
