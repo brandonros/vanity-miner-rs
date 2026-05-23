09155ef8952f979bac55ea4ddda35e40552b6cc2
Brandon Ros <brandonros1@gmail.com>
2026-05-23 17:58:15 -0400

    cpu_workers

--- cli/src/common/mod.rs
+++ cli/src/common/mod.rs
@@ -1,15 +1,17 @@
 mod stats;
 mod validation;
+#[cfg(feature = "shallenge")]
 mod shared_best_hash;
 #[cfg(not(feature = "gpu"))]
-mod workers;
+mod cpu_workers;
 #[cfg(feature = "gpu")]
 mod gpu_context;
 
 pub use stats::*;
+#[cfg(feature = "shallenge")]
 pub use shared_best_hash::*;
 pub use validation::*;
 #[cfg(not(feature = "gpu"))]
-pub use workers::*;
+pub use cpu_workers::*;
 #[cfg(feature = "gpu")]
 pub use gpu_context::GpuContext;
