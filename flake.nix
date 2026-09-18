bb9a0a9d044f471b00cfbc43bde87ba5599734ad
Brandon Ros <brandonros1@gmail.com>
2026-09-18 18:45:03 -0400

    Add an opt-in selective-inlining Metal build path

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Stock Rust and LLVM-to-Metal development environment";
-  inputs.llvm-metal.url = "github:brandonros/llvm-metal/9cbb88e2b54e4719fb833eca14fb5f41421ebad0";
+  inputs.llvm-metal.url = "github:brandonros/llvm-metal/56c06a1f226132bb3d4f756e6d10e486ed8d34cf";
 
   outputs = { llvm-metal, ... }: {
     devShells = builtins.mapAttrs (system: shells:
